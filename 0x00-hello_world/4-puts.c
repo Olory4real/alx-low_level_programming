6324403fcff0a155c326b7a26c11553b4f465bad
Olory4real <monsurahxyz@gmail.com>
2022-12-08 14:12:23 -0800

    5-printf.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Description: main - prints out a string to stdout.
+ *
+ *Return: 0        
+ */
+
+int main(void)
+
+{	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}						
