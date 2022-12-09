4abc7e149207accdee2c2f6b19a3fad3fb0e2465
Olory4real <monsurahxyz@gmail.com>
2022-12-08 21:37:46 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Description: main - prints out a string to stdout.
- *Return: 0        
+ * main -main block
+ * Return: o
  */
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}						
+}
