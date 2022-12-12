e1451ea8dbacd5beca172f6c86d15a8c851ff8e9
Olory4real <monsurahxyz@gmail.com>
2022-12-12 06:53:21 -0800

    1-last_digit.c
    git commit -m 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,11 +16,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
-	return (0);
 	if (m > 5)
-		printf("Last digit of %d is %d and is greater than 5\n ", n, m);
-	if (m == 0)
-		printf("Last digit of %d is %d and is 0\n", n, m);
-	if (m < 6 && m != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+		printf("Last digit of %i is %i and is greater than 5\n", n, m);
+	else if  (m == 0)
+		printf("Last digit of %i is %i and is 0\n", n, m);
+	else if (m < 6)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", 		n, m);
+		
+	return (0);
 }
