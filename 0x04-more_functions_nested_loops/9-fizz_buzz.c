92aaae825e6cbdfcd9b4ea1867a76d86953301b5
Olory4real <monsurahxyz@gmail.com>
2022-12-16 12:14:13 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,27 +8,7 @@ int main(void)
 {
 	int n;
 
-	n = 1;
-	printf("%d", n);
+	n=1;
+	printf("%d", n0;
 	for (n = 2; n <= 100; n++)
-	{
-		if ((n % 3 == 0) && (n % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if (n % 3 == 0)
-		{
-		 printf("Fizz");
-		}
-		else if (n % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf("%d", n);
-		}
-	}
-	printf("\n");
-	return (0);
-}
+	
