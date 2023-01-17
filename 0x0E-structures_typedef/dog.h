0172d248f144d540db140ed7556f0fc50152e5e8
Olory4real <monsurahxyz@gmail.com>
2023-01-17 15:31:14 -0800

    task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef STRTYP
+#define STRTYP
 
 /**
  * struct dog - a dog's basic info
@@ -24,9 +24,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
-#endif
+
+#endif/*STRTYP*/
 
 
