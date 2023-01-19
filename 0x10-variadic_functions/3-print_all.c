eeb9085afe877a6a5e875482c492f3f0d9e4be72
Olory4real <monsurahxyz@gmail.com>
2023-01-19 04:22:22 -0800

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -21,22 +21,22 @@ void print_all(const char * const format, ...)
 		{
 			switch (format[i])
 			{
-				case 'c';
+				case 'c':
 					printf("%s%c", sep, va_arg(list, int));
 					break;
-				case 'i'
+				case 'i':
 					printf("%s%d", sep, va_arg(list, int));
 					break;
-				case 'f'
+				case 'f':
 					printf("%s%f", sep, va_arg(list, double));
 					break;
-				case 's'
+				case 's':
 					str = va_arg(list, char *);
 					if (!str)
 						str = "(nil)";
 					printf("%s%s", sep, str);
 					break;
-				default;
+				default:
 					i++;
 					continue;
 			}
